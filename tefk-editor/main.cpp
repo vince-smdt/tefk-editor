1f3efaa5785726b060886309c5dba69ea345a27a
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2022-11-22 19:23:02 -0500

    Create handleKeyPress function

--- tefk-editor/main.cpp
+++ tefk-editor/main.cpp
@@ -3,26 +3,35 @@
 #include <vector>
 #include <Windows.h>
 
+void handleKeyPress(std::string &contents);
+
 int main() {
 	std::string contents = "";
 	char ch;
 
 	while (true) {
-		ch = _getch();
-
-		if (ch == VK_BACK) {
-			if (contents.size()) contents.erase(contents.size() - 1);
+		if (_kbhit()) {
+			handleKeyPress(contents);
+			system("cls");
+			std::cout << contents;
 		}
-		else if (ch == VK_RETURN) {
-			contents += '\n';
-		}
-		else {
-			contents += ch;
-		}
-
-		system("cls");
-		std::cout << contents;
 	}
 
 	return 0;
+}
+
+void handleKeyPress(std::string &contents) {
+	char ch = _getch();
+
+	switch (ch) {
+	case VK_BACK:
+		if (contents.size()) contents.erase(contents.size() - 1);
+		break;
+	case VK_RETURN:
+		contents += '\n';
+		break;
+	default:
+		contents += ch;
+		break;
+	}
 }
