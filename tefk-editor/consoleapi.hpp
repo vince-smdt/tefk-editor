263039723e7329af04fa6023ae867ef6673545d5
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2022-12-13 16:08:00 -0500

    Create ConsoleAPI class

---
+++ tefk-editor/consoleapi.hpp
@@ -0,0 +1,82 @@
+#pragma once
+#include <Windows.h>
+#include "logger.hpp"
+
+namespace tefk {
+
+// TODO - Move this to another file
+enum WinConsoleTextColor {
+	BLACK = 0x00,
+	BLUE = 0x01,
+	GREEN = 0x02,
+	AQUA = 0x03,
+	RED = 0x04,
+	PURPLE = 0x05,
+	YELLOW = 0x06,
+	WHITE = 0x07,
+	GRAY = 0x08,
+	LIGHT_BLUE = 0x09,
+	LIGHT_GREEN = 0x0a,
+	LIGHT_AQUA = 0x0b,
+	LIGHT_RED = 0x0c,
+	LIGHT_PURPLE = 0x0d,
+	LIGHT_YELLOW = 0x0e,
+	BRIGHT_WHITE = 0x0f,
+};
+
+// TODO - Move this to another file
+typedef struct TextColor {
+	WinConsoleTextColor backgroundColor;
+	WinConsoleTextColor foregroundColor;
+} TextColor;
+
+class ConsoleAPI {
+private:
+	ConsoleAPI() = delete;
+	ConsoleAPI(const ConsoleAPI&) = delete;
+
+	static HANDLE s_handle;
+	static CONSOLE_SCREEN_BUFFER_INFO s_csbi;
+public:
+	static int RowCount() {
+		GetConsoleScreenBufferInfo(s_handle, &s_csbi);
+		return s_csbi.srWindow.Bottom - s_csbi.srWindow.Top + 1;
+	}
+
+	static int ColCount() {
+		GetConsoleScreenBufferInfo(s_handle, &s_csbi);
+		return s_csbi.srWindow.Right - s_csbi.srWindow.Left + 1;
+	}
+
+	static int CursorRowPos() {
+		GetConsoleScreenBufferInfo(s_handle, &s_csbi);
+		return s_csbi.dwCursorPosition.Y;
+	}
+
+	static int CursorColPos() {
+		GetConsoleScreenBufferInfo(s_handle, &s_csbi);
+		return s_csbi.dwCursorPosition.X;
+	}
+
+	static void SetCursorPos(int row, int col) {
+		COORD pos = { col, row };
+		SetConsoleCursorPosition(s_handle, pos);
+	}
+
+	static void SetTextColor(TextColor color) {
+		int colorCode = color.foregroundColor + color.backgroundColor * 0x10;
+		SetConsoleTextAttribute(s_handle, colorCode);
+	}
+
+	static void SetConsoleSize(int row, int col) {
+		COORD size = { col, row };
+		if (!SetConsoleScreenBufferSize(s_handle, size)) {
+			Logger::Instance().Log("SetConsoleScreenBufferSize() failed! Reason : {}", GetLastError());
+			exit(0);
+		}
+	}
+};
+HANDLE ConsoleAPI::s_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+CONSOLE_SCREEN_BUFFER_INFO ConsoleAPI::s_csbi;
+
+}
--- tefk-editor/consolemanager.hpp
+++ tefk-editor/consolemanager.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
+#include "consoleapi.hpp"
 #include "editor.hpp"
-#include "logger.hpp"
 
 namespace tefk {
 
@@ -10,100 +10,43 @@ private:
 	ConsoleManager() = delete;
 	ConsoleManager(const ConsoleManager&) = delete;
 
-	static HANDLE _handle;
-	static CONSOLE_SCREEN_BUFFER_INFO _csbi;
-	static int _currRows;
-	static int _currCols;
+	static int s_currRows;
+	static int s_currCols;
 
-	// TODO - Move this enum to some other file
-	enum WinConsoleTextColor {
-		BLACK =         0x00,
-		BLUE =          0x01,
-		GREEN =         0x02,
-		AQUA =          0x03,
-		RED =           0x04,
-		PURPLE =        0x05,
-		YELLOW =        0x06,
-		WHITE =         0x07,
-		GRAY =          0x08,
-		LIGHT_BLUE =    0x09,
-		LIGHT_GREEN =   0x0a,
-		LIGHT_AQUA =    0x0b,
-		LIGHT_RED =     0x0c,
-		LIGHT_PURPLE =  0x0d,
-		LIGHT_YELLOW =  0x0e,
-		BRIGHT_WHITE =  0x0f,
-	};
-
-	typedef struct TextColor {
-		WinConsoleTextColor backgroundColor;
-		WinConsoleTextColor foregroundColor;
-	} TextColor;
-
+	// TODO - make these constants
 	static TextColor _defaultColor;
-
 	static TextColor _headerColor;
 	static TextColor _contentColor;
 	static TextColor _footerColor;
-
 public:
-	static int RowCount() {
-		GetConsoleScreenBufferInfo(_handle, &_csbi);
-		return _csbi.srWindow.Bottom - _csbi.srWindow.Top + 1;
-	}
-
-	static int ColCount() {
-		GetConsoleScreenBufferInfo(_handle, &_csbi);
-		return _csbi.srWindow.Right - _csbi.srWindow.Left + 1;
-	}
-
-	static int CursorRowPos() {
-		GetConsoleScreenBufferInfo(_handle, &_csbi);
-		return _csbi.dwCursorPosition.Y;
-	}
-
-	static int CursorColPos() {
-		GetConsoleScreenBufferInfo(_handle, &_csbi);
-		return _csbi.dwCursorPosition.X;
-	}
-
 	static bool ConsoleSizeChanged() {
-		return _currRows != RowCount() || _currCols != ColCount();
-	}
-
-	static void SetCursorPos(int row, int col) {
-		COORD pos = { col, row };
-		SetConsoleCursorPosition(_handle, pos);
-	}
-
-	static void SetTextColor(TextColor color) {
-		int colorCode = color.foregroundColor + color.backgroundColor * 0x10;
-		SetConsoleTextAttribute(_handle, colorCode);
+		return s_currRows != ConsoleAPI::RowCount() || s_currCols != ConsoleAPI::ColCount();
 	}
 
 	static void Print(std::string text, TextColor color) {
-		SetTextColor(color);
+		ConsoleAPI::SetTextColor(color);
 
-		if (text.size() > ColCount())
-			text = text.substr(0, ColCount() - 1);
+		if (text.size() > ConsoleAPI::ColCount())
+			text = text.substr(0, ConsoleAPI::ColCount() - 1);
 		std::cout << text << FillRow;
 
-		SetTextColor(_defaultColor);
+		ConsoleAPI::SetTextColor(_defaultColor);
 	}
 
 	static void PrintHeader() {
-		SetCursorPos(0, 0);
+		ConsoleAPI::SetCursorPos(0, 0);
 
 		std::stringstream ss;
 		ss << Editor::CurrentFile().GetFilename() << " "
 		   << Editor::FileIndex() + 1 << "/"
-		   << Editor::Files().size();
+		   << Editor::Files().size() << " "
+		   << "Press Ctrl+S to save!";
 		Print(ss.str(), _headerColor);
 	}
 
 	static void PrintContent() {
 		// TODO - Only print content that fits in the console, rest can be seen by scrolling
-		SetCursorPos(2, 0);
+		ConsoleAPI::SetCursorPos(2, 0);
 
 		std::stringstream ss;
 		ss << Editor::CurrentFile().GetContent();
@@ -111,29 +54,24 @@ public:
 	}
 
 	static void PrintFooter() {
-		SetCursorPos(RowCount() - 1, 0);
+		ConsoleAPI::SetCursorPos(ConsoleAPI::RowCount() - 1, 0);
 
 		std::stringstream ss;
-		ss << "Rows = " << ConsoleManager::RowCount() 
-		   << ", Cols = " << ConsoleManager::ColCount();
+		ss << "Rows = " << ConsoleAPI::RowCount() 
+		   << ", Cols = " << ConsoleAPI::ColCount();
 		Print(ss.str(), _footerColor);
 	}
 
 	static std::ostream& FillRow(std::ostream& stream) {
-		return stream << std::string(ColCount() - CursorColPos(), ' ');
+		return stream << std::string(ConsoleAPI::ColCount() - ConsoleAPI::CursorColPos(), ' ');
 	}
 
 	static void RefreshConsole() {
 		// TODO - Reset entire console color in case previous color before app starts
-		_currRows = RowCount();
-		_currCols = ColCount();
+		s_currRows = ConsoleAPI::RowCount();
+		s_currCols = ConsoleAPI::ColCount();
 
-		COORD consoleSize = { _currCols, _currRows };
-		if (!SetConsoleScreenBufferSize(_handle, consoleSize)) {
-			// TODO - make logger callable from anywhere without having to initialize logger object
-			Logger::Instance().Log("SetConsoleScreenBufferSize() failed! Reason : {}", GetLastError());
-			exit(0);
-		}
+		ConsoleAPI::SetConsoleSize(s_currRows, s_currCols);
 
 		// TODO - Code more efficient function to clear console
 		system("cls");
@@ -143,13 +81,11 @@ public:
 		PrintFooter();
 	}
 };
-HANDLE ConsoleManager::_handle = GetStdHandle(STD_OUTPUT_HANDLE);
-CONSOLE_SCREEN_BUFFER_INFO ConsoleManager::_csbi;
-int ConsoleManager::_currRows = 0;
-int ConsoleManager::_currCols = 0;
-ConsoleManager::TextColor ConsoleManager::_defaultColor = { BLACK, WHITE };
-ConsoleManager::TextColor ConsoleManager::_headerColor = { WHITE, BLACK };
-ConsoleManager::TextColor ConsoleManager::_contentColor = { BLACK, WHITE };
-ConsoleManager::TextColor ConsoleManager::_footerColor = { WHITE, BLACK };
+int ConsoleManager::s_currRows = 0;
+int ConsoleManager::s_currCols = 0;
+TextColor ConsoleManager::_defaultColor = { BLACK, WHITE };
+TextColor ConsoleManager::_headerColor = { WHITE, BLACK };
+TextColor ConsoleManager::_contentColor = { BLACK, WHITE };
+TextColor ConsoleManager::_footerColor = { WHITE, BLACK };
 
 }
