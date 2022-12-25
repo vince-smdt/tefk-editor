e6ece2cf8c7503e59c38be0e71f549b4e01048e4
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2022-12-25 16:36:50 -0500

    Move gui components into components folder

--- tefk-editor/designer.hpp
+++ tefk-editor/designer.hpp
@@ -1,9 +1,9 @@
 #pragma once
 #include "editor.hpp"
-#include "gui/window.hpp"
-#include "gui/panel.hpp"
-#include "gui/label.hpp"
-#include "gui/textfield.hpp"
+#include "gui/components/window.hpp"
+#include "gui/components/panel.hpp"
+#include "gui/components/label.hpp"
+#include "gui/components/textfield.hpp"
 
 namespace tefk::Designer {
 	std::shared_ptr<Window> Editor();
