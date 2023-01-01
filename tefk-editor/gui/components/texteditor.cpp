c9d925a85dde2f3faaafd411f58f4c333ce42960
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2023-01-01 11:59:58 -0500

    Make text editor component store text in rows of strings with std::vector

--- tefk-editor/gui/components/texteditor.cpp
+++ tefk-editor/gui/components/texteditor.cpp
@@ -11,37 +11,32 @@ TextEditor::TextEditor(Coord pos, Coord size, TextColor color)
 {}
 
 void TextEditor::SetText(std::string text) {
-	_text = text;
+	size_t currLineBreak = 0,
+	       nextLineBreak = 0;
+
+	_rows.clear();
+	while (true) {
+		nextLineBreak = text.find_first_of('\n', currLineBreak);
+		_rows.push_back(text.substr(currLineBreak, nextLineBreak - currLineBreak));
+		if (nextLineBreak == std::string::npos)
+			break;
+		currLineBreak = ++nextLineBreak;
+	};
 }
 
 void TextEditor::PrintContent() {
-	// TODO - This is the same as the PrintContent method of textfield component,
-	//		  either make parent "Text" class for both or modify this method
-
-	// Print label
-	size_t offsetIndex = 0,
-	       nextBreakingIndex = 0;
-
-	for (short currRow = 0; currRow < _size.Y && currRow + _pos.Y < ConsoleAPI::RowCount(); currRow++) {
+	for (size_t currRow = 0; currRow < _size.Y && currRow + _pos.Y < ConsoleAPI::RowCount(); currRow++) {
 		ConsoleAPI::SetCursorPos(_pos.Y + currRow, _pos.X);
 
-		// Find next breaking char
-		for (size_t currRowIndex = 0; currRowIndex + offsetIndex <= _text.size() && currRowIndex < _size.X; currRowIndex++) {
-			nextBreakingIndex = currRowIndex + offsetIndex;
-			if (currRowIndex + offsetIndex == _text.size() || _text[currRowIndex + offsetIndex] == '\n')
-				break;
-		}
-
-		// Print entire row
-		// TODO - reduce number of std::strings being constructed each loop iteration
-		std::string rowText = _text.substr(offsetIndex, nextBreakingIndex - offsetIndex);
-		std::string row = rowText + std::string(_size.X - rowText.size(), ' ');
+		std::string rowText = (currRow < _rows.size())
+			? _rows[currRow]
+			: "";
+		
+		std::string row = (rowText.size() <= _size.X)
+			? rowText + std::string(_size.X - rowText.size(), ' ')
+			: rowText.substr(0, _size.X);
+		
 		std::cout.write(row.c_str(), row.size());
-
-		// Set text offset for next iteration
-		if (_text[nextBreakingIndex] == '\n')
-			nextBreakingIndex++;
-		offsetIndex = nextBreakingIndex;
 	}
 }
 
--- tefk-editor/gui/components/texteditor.hpp
+++ tefk-editor/gui/components/texteditor.hpp
@@ -1,10 +1,13 @@
 #pragma once
+#include <list>
 #include "guicomponent.hpp"
 
 namespace tefk {
 
 class TextEditor : public GUIComponent {
-	std::string _text;
+	std::vector<std::string> _rows;
+	std::vector<std::string>::iterator _cursorRow;
+	std::string::iterator _cursorCol;
 public:
 	TextEditor();
 	TextEditor(Coord pos, Coord size, TextColor color);
