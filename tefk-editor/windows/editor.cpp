8043d14373f1d3a4e10218754161fb66f9315da2
Vincent Simard-Schmidt <vince.smdt@gmail.com>
2023-01-15 16:49:59 -0500

    Merge editor designer file to main implementation file

--- tefk-editor/windows/editor.cpp
+++ tefk-editor/windows/editor.cpp
@@ -2,6 +2,25 @@
 
 namespace tefk {
 
+Editor::Editor() {
+	// Configure all components
+	_lblHeader.SetColor({ BLACK, WHITE });
+
+	_ediEditor.SetColor({ WHITE, BLACK });
+
+	_txtFilename.SetColor({ WHITE, LIGHT_RED });
+	_txtFilename.SetLabel("Choose filename");
+	_txtFilename.SetVisible(false);
+
+	_lblFooter.SetColor({ BLACK, WHITE });
+
+	// Add all components to window
+	AddComponent(_lblHeader);
+	AddComponent(_ediEditor);
+	AddComponent(_txtFilename);
+	AddComponent(_lblFooter);
+}
+
 void Editor::CatchEvent(Event& event) {
 	// Handle event
 	if (event.type == Event::Type::CHARACTER) {
